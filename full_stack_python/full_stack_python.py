f824ff20df62ee7c3f76bc7e4525d2be98c5296a
henryantwi <henryantwi191@gmail.com>
2024-09-12 02:44:19 +0000

    Refactor .gitignore and assets/external/ directory
    - Remove trailing whitespace in .gitignore
    - Add missing newline at the end of .gitignore
    - Remove unused assets/external/ directory
    
    Refactor full_stack_python module
    
    - Update State class in full_stack_python.py
      - Add label attribute
      - Add current_val attribute
      - Add handle_title_input_change method
      - Add did_click method
    - Update index function in full_stack_python.py
      - Update welcome message to use State.label
      - Add input field with default value from State.label
      - Add on_click and on_change event handlers to input field
    - Remove unused full_stack_python/ui/__init__.py file
    - Update base_page function in full_stack_python/ui/base.py
      - Remove navbar parameter
      - Add hide_navbar parameter
      - Update function logic to conditionally hide navbar
    - Add navbar module in full_stack_python/ui/nav.py
      - Add navbar_link function
      - Add navbar function
    
    Add logo image
    
    - Add logo.jpg to assets directory

--- full_stack_python/full_stack_python.py
+++ full_stack_python/full_stack_python.py
@@ -4,23 +4,36 @@ import reflex as rx
 
 from rxconfig import config
 
+from .ui.base import base_page
+
 
 class State(rx.State):
     """The app state."""
 
-    ...
+    label = "Welcome to Reflex!"
+    current_val = 0
+
+    def handle_title_input_change(self, val):
+        self.label = val
+
+    def did_click(self):
+        print(">>>>>> did clicked was clicked! <<<<<<<<")
 
 
 def index() -> rx.Component:
     # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="bottom-left"),
+    return base_page(
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
+            rx.heading(State.label, size="9"),
             rx.text(
                 "Get started by editing ",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
                 size="5",
+            ),
+            rx.input(
+                default_value=State.label,
+                on_click=State.did_click,
+                on_change=State.handle_title_input_change,
             ),
             rx.link(
                 rx.button("Check out our docs!"),
@@ -31,7 +44,7 @@ def index() -> rx.Component:
             justify="center",
             min_height="85vh",
         ),
-        rx.logo(),
+        # hide_navbar=True,
     )
 
 
---
+++ full_stack_python/ui/base.py
@@ -0,0 +1,22 @@
+import reflex as rx
+
+from .nav import navbar
+
+
+def base_page(child: rx.Component, hide_navbar=False, *args, **kwargs) -> rx.Component:
+    # print([type(x) for x in args])
+    if not isinstance(child, rx.Component):
+        child = rx.heading("this is not a valid child element")
+    if hide_navbar:
+        return rx.container(
+            # navbar(), # Navbar removed!
+            child,
+            rx.color_mode.button(position="bottom-left"),
+            rx.logo(),
+        )
+    return rx.container(
+        navbar(),
+        child,
+        rx.color_mode.button(position="bottom-left"),
+        rx.logo(),
+    )
